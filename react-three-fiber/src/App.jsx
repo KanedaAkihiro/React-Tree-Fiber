71173cd7cb9331f35f1e23e870a857d75de8c72b
Akihiro Kaneda <101224342+KanedaAkihiro@users.noreply.github.com>
2023-06-28 23:42:39 +0900

    Add: 3Dオブジェクトの調整 ・rectAreaLight：青光と光彩調整 ・PresentationControls: 3Dオブジェクトの調整 ・globalプロパティ: 慣性を追加 ・cofig: ３Dオブジェクトに浮遊感を追加 ・snap: ３Dオブジェクトドラック後の視点回復

--- react-three-fiber/src/App.jsx
+++ react-three-fiber/src/App.jsx
@@ -1,28 +1,45 @@
-import { OrbitControls, useGLTF } from "@react-three/drei";
-import './App.css'
-import { Canvas } from "react-three-fiber"
+import {
+  OrbitControls,
+  PresentationControls,
+  useGLTF,
+} from "@react-three/drei";
+import "./App.css";
+import { Canvas } from "react-three-fiber";
 
 function App() {
-
   const macbook = useGLTF(
     "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf"
   );
   return (
     <div>
-      <Canvas camera={{fov: 45, near:0.1, far: 2000}}>
+      <Canvas camera={{ fov: 45, near: 0.1, far: 2000 }}>
         <color args={["#155196"]} attach="background" />
         {/* ジオメトリを立体的に稼働させるライブラリ */}
-        <OrbitControls />
+        {/* <OrbitControls /> */}
         {/* ジオメトリの明細工場 */}
         <ambientLight />
         {/* <mesh>
           <boxGeometry />
           <meshStandardMaterial />
         </mesh> */}
-        <primitive object={macbook.scene} position={[0,-1,0]}></primitive>
+        <PresentationControls
+          global
+          config={{ mass: 2, tenstion: 400 }}
+          snap={{ mass: 4, tenstion: 300 }}
+        >
+          <rectAreaLight
+            color={"#0021a7"}
+            intensity={55}
+            rotation={[0.1, Math.PI, 0]}
+            width={2.0}
+            height={1.65}
+            position={[0, 0, -1]}
+          />
+          <primitive object={macbook.scene} position={[0, -1, 0]}></primitive>
+        </PresentationControls>
       </Canvas>
     </div>
   );
 }
 
-export default App
+export default App;
