5fe865fe59dbbdec9b434cf71c8ebcc557a0e2de
Akihiro Kaneda <101224342+KanedaAkihiro@users.noreply.github.com>
2023-06-28 23:55:03 +0900

    Add: Floatプロパティ ・初期表示時に３Dオブジェクトが自動で浮遊するように制御

--- react-three-fiber/src/App.jsx
+++ react-three-fiber/src/App.jsx
@@ -1,4 +1,5 @@
 import {
+  Float,
   OrbitControls,
   PresentationControls,
   useGLTF,
@@ -22,20 +23,23 @@ function App() {
           <boxGeometry />
           <meshStandardMaterial />
         </mesh> */}
+
         <PresentationControls
           global
           config={{ mass: 2, tenstion: 400 }}
           snap={{ mass: 4, tenstion: 300 }}
         >
-          <rectAreaLight
-            color={"#0021a7"}
-            intensity={55}
-            rotation={[0.1, Math.PI, 0]}
-            width={2.0}
-            height={1.65}
-            position={[0, 0, -1]}
-          />
-          <primitive object={macbook.scene} position={[0, -1, 0]}></primitive>
+          <Float rocationIndensity={1.6}>
+            <rectAreaLight
+              color={"#0021a7"}
+              intensity={55}
+              rotation={[0.1, Math.PI, 0]}
+              width={2.0}
+              height={1.65}
+              position={[0, 0, -1]}
+            />
+            <primitive object={macbook.scene} position={[0, -1, 0]}></primitive>
+          </Float>
         </PresentationControls>
       </Canvas>
     </div>
